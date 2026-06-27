cf0f6bd4f0fda4dd121e432572252a0f71004b0c
fratellipizzaoficial <94076866+MarcusSFerraz@users.noreply.github.com>
2026-06-27 20:21:09 -0300

    Retirando o processo de remover valor da carteira

--- src/main/java/com/zenin/service/InvestimentoService.java
+++ src/main/java/com/zenin/service/InvestimentoService.java
@@ -67,9 +67,6 @@ public class InvestimentoService {
                 .ativo(true)
                 .build();
 
-        carteira.setSaldoAtual(carteira.getSaldoAtual().subtract(valorInicial));
-        carteiraRepository.save(carteira);
-
         return investimentoRepository.save(investimento);
     }
 
